37dfc39258653ab231ed234f22ae5749c826cd02
Tyler Hall <apple@Tylers-MacBook-Pro.local>
2015-04-08 01:12:52 -0400

    setting up auth files and structure

--- server.js
+++ server.js
@@ -81,6 +81,20 @@ router.route('/users')
 //single user routes
 
 router.route('/:username')
+
+  .post(function(req,res){
+    //setup new user and add pertinent information
+    var user = new User();//new isntance of user model (users.js)
+    user.username = req.body.username;
+    user.passwd = req.body.passwd;
+    user.save(function(err){
+      if(err){
+        res.send(err);
+      }
+      res.json({message: 'User ' + req.body.username + ' Added!'});
+    });
+  })//end new user post operation
+
   //get operation
   .get(function(req,res){
     //in query use req.params when hooked to frontend
